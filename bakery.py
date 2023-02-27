3042cdb0f8f49ce2699473180ea02fea29d12a61
simonkrupa <simon.krupa@icloud.com>
2023-02-27 17:43:20 +0100

    feat: implement bakery algorithm

---
+++ bakery.py
@@ -0,0 +1,51 @@
+"""
+This module is implementation of Lamport's Bakery Algorithm.
+Module works for multiple threads.
+Threads can run multiple times.
+"""
+
+__author__ = "Šimon Krupa, Tomáš Vavro"
+__email__ = "xkrupas@stuba.sk"
+__license__ = "MIT"
+
+from fei.ppds import Thread
+
+NUM_THREADS = 8
+num = [0] * NUM_THREADS
+in_ = [0] * NUM_THREADS
+
+
+def process(tid, num_runs=1):
+    """
+    Simulates a process.
+    Arguments:
+        tid      -- thread id
+        num_runs -- number of executions of the critical section, if not declared set to 1
+    """
+    global num
+    global in_
+
+    for n in range(num_runs):
+        i = tid
+        in_[i] = 1
+        num[i] = 1 + max(num)
+        in_[i] = 0
+
+        for j in range(NUM_THREADS):
+            while in_[j] == 1:
+                continue
+
+            # wait for other processes to finish their execution of critical section
+            while num[j] != 0 and (num[j] < num[i] or (num[j] == num[i] and j < i)):
+                continue
+
+        # execute critical section
+        print(f"Process {tid} runs a complicated computation!")
+
+        # exit critical section
+        num[i] = 0
+
+
+if __name__ == '__main__':
+    threads = [Thread(process, i, 2) for i in range(NUM_THREADS)]
+    [t.join() for t in threads]
