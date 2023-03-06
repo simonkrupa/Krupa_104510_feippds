079d94be507aaaa069b1fe71117d88ab8e357c4d
simonkrupa <simon.krupa@icloud.com>
2023-03-06 22:42:43 +0100

    docs: add comments

--- barber.py
+++ barber.py
@@ -19,6 +19,11 @@ from random import randint
 class Shared(object):
 
     def __init__(self):
+        """
+        Class constructor initialize creates 1 semaphore for multiplex
+        and 2 semaphore's for barber and customer states,
+        creates Mutex object, and waiting room counter
+        """
         self.mutex = Mutex()
         self.mutex2 = Mutex()
         self.mutex3 = Mutex()
@@ -29,25 +34,53 @@ class Shared(object):
 
 
 def get_haircut(i):
+    """
+    Prints information about customer that is getting a haircut.
+
+    :param i: id of customer
+    """
     print(f'\nCustomer {i} getting a haircut')
 
 
 def cut_hair():
+    """
+    Prints information about barber that is cutting a haircut,
+    invoke sleep to simulate process of cutting.
+    """
     print("\nBarber cuts a haircut")
     sleep(randint(2,4))
 
 
 def balk(i):
+    """
+    Prints information about customer that is waiting for space in waiting room,
+    invoke sleep to simulate waiting time.
+
+    :param i: id of customer
+    """
     print(f'Customer {i} entered, but waiting room is full')
     sleep(randint(2, 5))
 
 
 def growing_hair(i):
+    """
+    Prints information about customer that is leaving barbershop
+    and is waiting to grow his hair back,
+    sleep simulates waiting process.
+
+    :param i: id of customer
+    """
     print(f'Customer {i} left and is growing his hair')
     sleep(randint(4, 8))
 
 
 def customer(i, shared):
+    """
+    Function that creates functionality of customer in barbershop.
+
+    :param i: id of customer
+    :param shared: instance of object Shared
+    """
     while True:
         shared.mutex.lock()
         if shared.counter == N:
@@ -72,6 +105,11 @@ def customer(i, shared):
 
 
 def barber(shared):
+    """
+    Function that creates functionality of barber in barbershop.
+
+    :param shared: instance of object Shared
+    """
     while True:
         shared.barber.wait()
         cut_hair()
@@ -90,7 +128,9 @@ def main():
         t.join()
 
 
+# number of customers
 C = 5
+# number of seats in waiting room
 N = 3
 
 if __name__ == "__main__":
