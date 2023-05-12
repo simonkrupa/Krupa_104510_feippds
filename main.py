ada6cf36cebb983c8330e1279f861ac33d2fece2
simonkrupa <simon.krupa@icloud.com>
2023-05-12 21:15:00 +0200

    docs: adding comments

--- main.py
+++ main.py
@@ -14,13 +14,27 @@ import glob
 
 @cuda.jit
 def transform_to_grayscale_gpu(pixels, gray_image):
+    """Transform all pixels to grayscale using GPU.
+    Check bounds of  data.
+    Use grid() instead of manual pos computation.
+
+    :param pixels: numpy array of rgb color
+    :param gray_image: result array of rgb color
+    """
     row, col = cuda.grid(2)
-    if row < len(pixels) and col < len(pixels[row]):  # check array bounds
+    # check array bounds
+    if row < len(pixels) and col < len(pixels[row]):
         r, g, b = pixels[row][col]
+        # calculate rgb
         gray_image[row][col] = int(0.299 * r + 0.587 * g + 0.114 * b)
 
 
 def transform_to_grayscale_cpu(pixels):
+    """Transform all pixels to grayscale using CPU.
+
+    :param pixels: numpy array of rgb color
+    :return: result array of rgb color
+    """
     height, width, c = pixels.shape
     # Prepare for output
     gray_image = [[0 for i in range(width)] for j in range(height)]
@@ -28,11 +42,16 @@ def transform_to_grayscale_cpu(pixels):
     for j in range(height):
         for i in range(width):
             r, g, b = pixels[j][i]
+            # calculate rgb
             gray_image[j][i] = int(0.299 * r + 0.587 * g + 0.114 * b)
     return gray_image
 
 
 def prepare_data_for_gpu(pixels):
+    """Prepare block dimensions for CUDA
+    :param pixels: numpy array of rgb color
+    :return: result array of rgb color
+    """
     tpb = (30, 30)
     bpg = (10, 10)
     height, width, c = pixels.shape
@@ -41,29 +60,44 @@ def prepare_data_for_gpu(pixels):
     return gray_image
 
 
-if __name__ == "__main__":
+def start_process():
+    """Function that start process of transforming.
+    It loads images and in for loop transform each image
+    by using GPU and by using CPU.
+    Measure time for each.
+    Save grayscale images.
+
+    """
     image_directory = "images"
+    # Load all images
     images_files = glob.glob(os.path.join(image_directory, "*.jpg"))
     for index, images_file in enumerate(images_files):
-        # GPU
+        # Part transforming image by GPU
         start = time.time()
         pixels = plt.imread(images_file)
+        # Resize image
         pixels = cv2.resize(pixels, (300, 300))
 
         gray_image = prepare_data_for_gpu(pixels)
 
-        plt.imsave("grayscaled_images/gs_image_gpu"+str(index+1)+".jpg", gray_image, format="jpg", cmap="gray")
+        plt.imsave("grayscaled_images/gs_image_gpu" + str(index + 1) + ".jpg", gray_image, format="jpg", cmap="gray")
         end = time.time()
-        print(f'Time to transform with gpu image{index+1}: {end - start}')
+        print(f'Time to transform with gpu image{index + 1}: {end - start}')
 
-        # CPU
+        # Part transforming image by CPU
         start = time.time()
         pixels = plt.imread(images_file)
+        # Resize image
         pixels = cv2.resize(pixels, (300, 300))
 
         gray_image = transform_to_grayscale_cpu(pixels)
-        plt.imsave("grayscaled_images/gs_image_cpu"+str(index+1)+".jpg", gray_image, format="jpg", cmap="gray")
+
+        plt.imsave("grayscaled_images/gs_image_cpu" + str(index + 1) + ".jpg", gray_image, format="jpg", cmap="gray")
         end = time.time()
-        print(f'Time to transform with cpu image{index+1}: {end - start}')
+        print(f'Time to transform with cpu image{index + 1}: {end - start}')
+
+
+if __name__ == "__main__":
+    start_process()
 
 
